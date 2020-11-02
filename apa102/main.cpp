c051f89c2d3484ce81faf56baf98973154f1c75a
Kåre <kaareod@gmail.com>
2020-11-02 09:27:22 +0100

    Initial setup

---
+++ apa102/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * apa102.cpp
+ *
+ * Created: 02/11/2020 09.20.53
+ * Author : Kaare
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
