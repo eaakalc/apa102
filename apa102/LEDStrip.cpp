f7650c047f4d0d1c1e530a4c354f003dda0174bb
Kåre <kaareod@gmail.com>
2020-11-11 11:36:56 +0100

    Can now add/remove leds from a grid Can transmit the led frame using SPI or bit banging

--- apa102/LEDStrip.cpp
+++ apa102/LEDStrip.cpp
@@ -5,36 +5,97 @@
 * Author: Kaare
 */
 #include "LEDStrip.h"
+#define F_CPU 16000000UL
 
 int size = 0;
-LED **leds = malloc(0);
+LED *leds;
+uint8_t useSPI = 0;
+uint8_t brightness;
 
-// default constructor
-LEDStrip::LEDStrip()
+LEDStrip::LEDStrip(uint8_t rows, uint8_t columns, uint8_t br, uint8_t comms)
 {
+	if(br < 32)
+		brightness = br;
+	else
+		brightness = 31;
+	
+	if(comms)
+	{
+		useSPI = 1;
+		
+		DDRB |= (1 << 2) | (1 << 3) | (1 << 5);
 
-} //LEDStrip
-
-// default destructor
-LEDStrip::~LEDStrip()
-{
-} //~LEDStrip
-
-void insertLED(LED *l, uint8_t _r, uint8_t _g, uint8_t _b, uint8_t _brightness)
-{
-	l->r = _r;
-	l->g = _g;
-	l->b = _b;
-	l->brightness = _brightness;
-}
-
-void addLED(uint8_t r, uint8_t g, uint8_t b, uint8_t brightness, int position)
-{
-	if(position > size)
-	{	
-		leds = realloc(leds, position * sizeof(LED*));
-		size = position;
+		// enable SPI, set as master, and clock to fosc/128
+		SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR1) | (1 << SPR0);
+	}
+	else
+	{
+		DDRB |= (1 << 3) | (1 << 5);
 	}
 	
-	insertLED(&leds[position - 1], r, g, b, brightness);
+	leds = new LED[rows * columns];
+	
+	for (int i = 0; i < rows * columns; i++)
+	{
+		setLED(i, 0, 0, 0);
+	}
+}
+
+LEDStrip::~LEDStrip()
+{
+	delete[] leds;
+}
+
+void setLED(uint8_t pos, uint8_t r, uint8_t g, uint8_t b)
+{
+	leds[pos]._r = r;
+	leds[pos]._g = g;
+	leds[pos]._b = b;
+}
+
+void clearLED(uint8_t pos)
+{
+	leds[pos]._r = 0;
+	leds[pos]._g = 0;
+	leds[pos]._b = 0;
+}
+
+void sendByte(uint8_t byte)
+{
+	uint8_t data = byte;
+	
+	if(useSPI)
+	{
+		SPDR = data;
+	
+		while(!(SPSR & (1 << SPIF)));
+	}
+	else
+	{
+		if(data & 128)
+			PORTB |= (1 << 3);
+		else
+			PORTB &= ~(1 << 3);
+			
+		data <<= 1;
+		
+		PORTB |= (1 << 5);
+		_delay_ms(1);
+		PORTB &= ~(1 << 5);
+	}
+}
+
+void sendFrames()
+{
+	sendByte(0);
+	
+	for (uint8_t i = 0; i < (sizeof(leds) / sizeof(LED)); i++)
+	{
+		sendByte(224 + brightness);
+		sendByte(leds[i]._b);
+		sendByte(leds[i]._g);
+		sendByte(leds[i]._r);
+	}
+	
+	sendByte(255);
 }
--- apa102/LEDStrip.h
+++ apa102/LEDStrip.h
@@ -11,11 +11,12 @@
 
 #include <stdlib.h>
 #include <stdint.h>
+#include "avr/io.h"
+#include <util/delay.h>
 
 typedef struct
 {
-	uint8_t r, g, b, brightness;
-	LED(uint8_t _r, uint8_t _g, uint8_t _b, uint8_t _brightness) : r(_r), g(_g), b(_b), brightness(_brightness){}
+	uint8_t _r, _g, _b;
 } LED;
 
 class LEDStrip
@@ -27,17 +28,17 @@ private:
 
 //functions
 public:
-	LEDStrip();
+	LEDStrip(uint8_t rows, uint8_t columns, uint8_t br, uint8_t comms);
 	~LEDStrip();
-	void addLED(uint8_t r, uint8_t g, uint8_t b, uint8_t brightness, int position);
-	void removeLED(int position);
-	void updateStrip();
+	void setLED(uint8_t pos, uint8_t r, uint8_t g, uint8_t b);
+	void clearLED(uint8_t pos);
+	void sendFrames();
 protected:
 private:
-	void insertLED(LED *l, uint8_t _r, uint8_t _g, uint8_t _b, uint8_t _brightness);
 	LEDStrip( const LEDStrip &c );
 	LEDStrip& operator=( const LEDStrip &c );
-
+	void sendByte(uint8_t byte);
+	void createFrame(uint8_t pos);
 }; //LEDStrip
 
 #endif //__LEDSTRIP_H__
